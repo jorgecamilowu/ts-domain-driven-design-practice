31bf628dfb6c1af3c34a7a78075643ae98d7294b
jorgewu <jorge.wu@optilyz.com>
2024-04-01 12:04:30 +0200

    Implement rest of rpc layers

--- interfaces/rpc/router.ts
+++ interfaces/rpc/router.ts
@@ -1,13 +1,39 @@
 import { PSQLTaskRepository } from "../../domain/taskManagement/repositories/PSQLTaskRepository";
+import { CreateTask } from "../../domain/taskManagement/useCases/CreateTask";
+import { DeleteTask } from "../../domain/taskManagement/useCases/DeleteTask";
 import { GetAllTasks } from "../../domain/taskManagement/useCases/GetAllTasks";
 import { GetTaskById } from "../../domain/taskManagement/useCases/GetTaskById";
+import { UpdateTask } from "../../domain/taskManagement/useCases/UpdateTask";
+import { Priority } from "../../domain/taskManagement/valueObjects/Priority";
 import { db } from "../../infrastructure/database";
+import { InMemoryIdempotencyStore } from "../../infrastructure/idempotency/InMemoryIdempotencyStore";
 import { publicProcedure, router } from "./trpc";
 import { z } from "zod";
 
+const taskRepository = new PSQLTaskRepository(db);
+const inMemoryIdempotencyStore = new InMemoryIdempotencyStore({});
+
 const appRouter = router({
+  createTask: publicProcedure
+    .input(
+      z.object({
+        requestId: z.string(),
+        task: z.object({
+          title: z.string(),
+          description: z.string().default(""),
+          dueDate: z.date().default(new Date()),
+          priority: z.nativeEnum(Priority).default(Priority.LOW),
+          completed: z.boolean().default(false),
+        }),
+      })
+    )
+    .mutation(async ({ input }) => {
+      const useCase = new CreateTask(taskRepository, inMemoryIdempotencyStore);
+
+      return useCase.execute(input.task, input.requestId);
+    }),
   taskList: publicProcedure.query(async () => {
-    const useCase = new GetAllTasks(new PSQLTaskRepository(db));
+    const useCase = new GetAllTasks(taskRepository);
 
     return useCase.execute();
   }),
@@ -18,8 +44,37 @@ const appRouter = router({
       })
     )
     .query(async ({ input }) => {
-      const useCase = new GetTaskById(new PSQLTaskRepository(db));
+      const useCase = new GetTaskById(taskRepository);
 
+      return useCase.execute(input.id);
+    }),
+  updateTask: publicProcedure
+    .input(
+      z.object({
+        id: z.string(),
+        updateWith: z.object({
+          title: z.string().optional(),
+          description: z.string().optional(),
+          dueDate: z.date().optional(),
+          priority: z.nativeEnum(Priority).optional(),
+          completed: z.boolean().optional(),
+          createdAt: z.date().optional(),
+        }),
+      })
+    )
+    .mutation(async ({ input }) => {
+      const useCase = new UpdateTask(taskRepository);
+
+      return useCase.execute(input.id, input.updateWith);
+    }),
+  deleteTask: publicProcedure
+    .input(
+      z.object({
+        id: z.string().min(1),
+      })
+    )
+    .mutation(async ({ input }) => {
+      const useCase = new DeleteTask(taskRepository);
       return useCase.execute(input.id);
     }),
 });
