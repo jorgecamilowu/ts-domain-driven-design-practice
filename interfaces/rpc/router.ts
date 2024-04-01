a478ddd3612efc71d816f11060cb910440df19b4
jorgewu <jorge.wu@optilyz.com>
2024-04-01 11:11:34 +0200

    Implement rpc layer for GetTaskById

--- interfaces/rpc/router.ts
+++ interfaces/rpc/router.ts
@@ -1,5 +1,6 @@
 import { PSQLTaskRepository } from "../../domain/taskManagement/repositories/PSQLTaskRepository";
 import { GetAllTasks } from "../../domain/taskManagement/useCases/GetAllTasks";
+import { GetTaskById } from "../../domain/taskManagement/useCases/GetTaskById";
 import { db } from "../../infrastructure/database";
 import { publicProcedure, router } from "./trpc";
 import { z } from "zod";
@@ -10,6 +11,17 @@ const appRouter = router({
 
     return useCase.execute();
   }),
+  taskById: publicProcedure
+    .input(
+      z.object({
+        id: z.string().min(1),
+      })
+    )
+    .query(async ({ input }) => {
+      const useCase = new GetTaskById(new PSQLTaskRepository(db));
+
+      return useCase.execute(input.id);
+    }),
 });
 
 export type AppRouter = typeof appRouter;
