b3687bcfdda11e04af89556b45f4b8919b43c4a3
jorgewu <jorge.wu@optilyz.com>
2024-04-10 08:12:04 +0200

    authorize only accounts with write permissions to delete tasks

--- packages/backend/interfaces/rpc/routes/deleteTaskRoute.ts
+++ packages/backend/interfaces/rpc/routes/deleteTaskRoute.ts
@@ -1,14 +1,36 @@
 import { z } from "zod";
 import type { DeleteTask } from "../../../domain/taskManagement/useCases/DeleteTask";
-import { publicProcedure } from "../trpc";
+import type { GetResourceAccessLevel } from "../../../domain/auth/useCases/GetResourceAccessLevel";
+import { protectedProcedure } from "../trpc";
+import { TRPCError } from "@trpc/server";
 
-export const deleteTaskRoute = (useCase: DeleteTask) =>
-  publicProcedure
+export const deleteTaskRoute = ({
+  GetResourceAccessLevel,
+  DeleteTask,
+}: {
+  GetResourceAccessLevel: GetResourceAccessLevel;
+  DeleteTask: DeleteTask;
+}) =>
+  protectedProcedure
     .input(
       z.object({
         id: z.number().min(1),
       })
     )
-    .mutation(async ({ input }) => {
-      return useCase.execute(input.id);
+    .mutation(async ({ input, ctx }) => {
+      const accessLevel = await GetResourceAccessLevel.execute(
+        ctx.account.role,
+        {
+          accountId: ctx.account.id,
+          name: "task",
+        }
+      );
+
+      if (accessLevel !== "readAndWrite" && accessLevel !== "write") {
+        throw new TRPCError({
+          code: "UNAUTHORIZED",
+        });
+      }
+
+      return DeleteTask.execute(input.id);
     });
