71471f808bda4fd4b68a077d936ef029fb9f61f4
jorgewu <jorge.wu@optilyz.com>
2024-04-21 11:52:41 +0200

    Implement idempotency in create account use case

--- packages/backend/domain/auth/useCases/CreateAccount.test.ts
+++ packages/backend/domain/auth/useCases/CreateAccount.test.ts
@@ -1,20 +1,25 @@
-import { beforeEach, describe, it, expect } from "bun:test";
+import { beforeEach, describe, it, expect, spyOn } from "bun:test";
 import { CreateAccount } from "./CreateAccount";
 import type { AccountRepository } from "../repositories/AccountRepository";
 import { InMemoryAccountRepository } from "../repositories/InMemoryAccountRepository";
 import type { NewAccount } from "../entities/AccountTable";
 import { BunPasswordHasher } from "../services/BunPasswordHasher";
+import { InMemoryIdempotencyStore } from "../../../infrastructure/idempotency/InMemoryIdempotencyStore";
+import type { IdempotencyStore } from "../../../infrastructure/idempotency/IdempotencyStore";
+import { nanoid } from "nanoid";
 
 describe("Create Account Use Case", () => {
   let useCase: CreateAccount;
   let accountRepository: AccountRepository;
+  let idempotencyStore: IdempotencyStore;
 
   beforeEach(() => {
+    idempotencyStore = new InMemoryIdempotencyStore({});
     accountRepository = new InMemoryAccountRepository(
       {},
       new BunPasswordHasher()
     );
-    useCase = new CreateAccount(accountRepository);
+    useCase = new CreateAccount(accountRepository, idempotencyStore);
   });
 
   it("creates a new account", async () => {
@@ -36,5 +41,33 @@ describe("Create Account Use Case", () => {
 
     // password should be hashed
     expect(account?.password).not.toEqual(accountPayload.password);
+  });
+
+  it("creates the new account only once", async () => {
+    spyOn(accountRepository, "create");
+
+    const accountPayload: NewAccount = {
+      email: "test@email.com",
+      name: "TestAccount",
+      password: "some-password",
+      roleId: 123,
+    };
+
+    const idempotencyKey = nanoid();
+
+    const id = await useCase.execute(accountPayload, idempotencyKey);
+    const secondTime = await useCase.execute(accountPayload, idempotencyKey);
+
+    expect(accountRepository.create).toHaveBeenCalledTimes(1);
+
+    expect(id).toBe(secondTime);
+
+    const firstFind = await accountRepository.findById(id);
+    const secondFind = await accountRepository.findById(secondTime);
+
+    expect(firstFind).not.toBeNull();
+    expect(secondFind).not.toBeNull();
+
+    expect(firstFind).toMatchObject({ ...secondFind });
   });
 });
--- packages/backend/domain/auth/useCases/CreateAccount.ts
+++ packages/backend/domain/auth/useCases/CreateAccount.ts
@@ -1,13 +1,28 @@
+import type { IdempotencyStore } from "../../../infrastructure/idempotency/IdempotencyStore";
 import type { NewAccount } from "../entities/AccountTable";
 import type { AccountRepository } from "../repositories/AccountRepository";
 
 export class CreateAccount {
-  constructor(private accountRepository: AccountRepository) {}
-  async execute(
-    account: NewAccount,
-    _idempotencyKey?: string
-  ): Promise<number> {
+  constructor(
+    private accountRepository: AccountRepository,
+    private idempotencyStore: IdempotencyStore
+  ) {}
+  async execute(account: NewAccount, idempotencyKey?: string): Promise<number> {
+    let result: number | undefined;
+
+    if (idempotencyKey !== undefined) {
+      result = await this.idempotencyStore.get<number>(idempotencyKey);
+    }
+
+    if (result !== undefined) {
+      return result;
+    }
+
     const id = await this.accountRepository.create(account);
+
+    if (idempotencyKey) {
+      this.idempotencyStore.set(idempotencyKey, id);
+    }
 
     return id;
   }
